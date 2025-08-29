15919b6eb25cadc39742f123f9b55a2f5ce82be9
beatrizquintino <beatriz_quintino@estudante.sesisenai.org.br>
2025-08-29 08:56:37 -0300

    exercicios php

--- exercicio17.php
+++ exercicio17.php
@@ -13,7 +13,10 @@
         <input type="number" id="dia" name="dia" required>
         <br>
         <label for="mes">Digite um mês:</label>
-        <input type="number" id="dia" name="dia" required>
+        <input type="number" id="mes" name="mes" required>
+        <br>
+        <label for="ano">Digite um ano:</label>
+        <input type="number" id="ano" name="ano" required>
         <br>
         <button type="submit" name="enviar">Enviar</button>
     </form>
@@ -21,7 +24,33 @@
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['enviar'])) {
-            $nome = $_POST['nome'];
+            $dia = $_POST['dia'];
+            $mes = $_POST['mes'];
+            $ano = $_POST['ano'];
+
+            if ($mes <= 12) {
+                if ($mes = 1 || $mes = 3 || $mes = 5 || $mes = 7 || $mes = 8 || $mes = 10 || $mes = 12) {
+                    if ($dia <= 31) {
+                        echo "Data válida";
+                    } else {
+                        echo "Data inválida";
+                    }
+                } else if ($mes = 4 || $mes = 6 || $mes = 9 || $mes = 11) {
+                    if ($dia <= 30) {
+                        echo "Data válida";
+                    } else {
+                        echo "Data inválida";
+                    }
+                } else if ($mes = 2) {
+                    if ($dia <= 29) {
+                        echo "Data válida";
+                    } else {
+                        echo "Data inválida";
+                    }
+                }
+            } else {
+                echo "Data inválida";
+            }
         }
     }
     ?>
